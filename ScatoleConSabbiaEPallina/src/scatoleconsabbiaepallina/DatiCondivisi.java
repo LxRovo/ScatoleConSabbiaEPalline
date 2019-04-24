0491b1b2d2e6ec3a831c25a279dd69b528d7da1a
mattia <mattia@alpha>
2019-04-24 13:54:12 +0200

    Aggiornamento classe DatiCondivisi pt.2

--- ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/DatiCondivisi.java
+++ ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/DatiCondivisi.java
@@ -14,13 +14,16 @@ public class DatiCondivisi {
     
     private int inclinazioneXScatola;
     private int inclinazioneYPallina;
+    private int inclinazioneXPallina;
     private int numScatole;
     private boolean running;
 
-    public DatiCondivisi(int numScatole, int inclinazioneXScatola,int inclinazioneYPallina) {
+    public DatiCondivisi(int numScatole, int inclinazioneXScatola,int inclinazioneYPallina,int inclinazioneXPallina) {
         this.numScatole = numScatole;
         this.inclinazioneXScatola = inclinazioneXScatola;
         this.inclinazioneYPallina= inclinazioneYPallina;
+        this.inclinazioneXPallina=inclinazioneXPallina;
+        
         running = true;
     }
 
@@ -43,6 +46,12 @@ public class DatiCondivisi {
 
     }
     
+    public synchronized int getInclinazioneXPallina() {
+
+        return inclinazioneXPallina;
+
+    }
+    
      public synchronized void setNumScatole(int numScatole) {
 
        this.numScatole=numScatole;
@@ -58,6 +67,12 @@ public class DatiCondivisi {
      public synchronized void setInclinazioneYPallina(int inclinazioneYPallina) {
 
         this.inclinazioneYPallina = inclinazioneYPallina;
+
+    }
+    
+      public synchronized void setInclinazioneXPallina(int inclinazioneXPallina) {
+
+        this.inclinazioneXPallina = inclinazioneXPallina;
 
     }
     
