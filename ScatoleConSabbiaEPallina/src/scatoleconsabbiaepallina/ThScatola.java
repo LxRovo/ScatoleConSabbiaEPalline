a5fae942fb453b5dbaaee89c3ee730fad4332621
Ziz <Ziz@DESKTOP-FV2DUF0>
2019-05-02 15:38:00 +0200

    Thread Scatola implementato

--- ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/ThScatola.java
+++ ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/ThScatola.java
@@ -7,20 +7,66 @@ package scatoleconsabbiaepallina;
 
 /**
  *
- * @author Ziz
+ * @author rovelli_andrea
  */
-public class ThScatola extends Thread{
-    
+public class ThScatola extends Thread {
+
+    private final float altezza;
+    private final float lunghezza;
+    private final float larghezza;
+    private final float raggioFinestre;
+    private float x;
+    private float y;
     private DatiCondivisi ptrDati;
-    private int idScatola;
-    
-    
-    public ThScatola(DatiCondivisi ptrDati, int idScatola) {
+    private Sabbia sabbia;
+
+    public ThScatola(float altezza, float x, float y, float lunghezza, float larghezza, DatiCondivisi ptrDati) {
+        this.altezza = altezza;
+        this.lunghezza = lunghezza;
+        this.larghezza = larghezza;
+        this.raggioFinestre = 20;
+        this.x = x;
+        this.y = y;
         this.ptrDati = ptrDati;
-        this.idScatola = idScatola;
+        
+    }
+    
+    @Override
+    public void run(){
+    
+        sabbia.move();
+    
+    }
+    
+
+    public float getAltezza() {
+        return altezza;
+    }
+
+    public float getLunghezza() {
+        return lunghezza;
+    }
+
+    public float getLarghezza() {
+        return larghezza;
+    }
+
+    public float getRaggioFinestre() {
+        return raggioFinestre;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public Sabbia getSabbia() {
+        return sabbia;
     }
     
     
-    
-   
+
 }
