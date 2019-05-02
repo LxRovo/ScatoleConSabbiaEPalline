cb59f60a889b75857dcf944596451d5a44b11d51
mattia <mattia@alpha>
2019-05-02 17:50:03 +0200

    Aggiornamento definitivo classe DatiCondivisi

--- ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/DatiCondivisi.java
+++ ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/DatiCondivisi.java
@@ -5,83 +5,57 @@
  */
 package scatoleconsabbiaepallina;
 
+import java.util.Vector;
+
 /**
  *
- * @author Ziz
+ * @author Mattia
  */
 public class DatiCondivisi {
 
-    
-    private int inclinazioneXScatola;
-    private int inclinazioneYPallina;
-    private int inclinazioneXPallina;
-    private int numScatole;
-    private boolean running;
+    private Vector<ThScatola> scatole;
+    private float inclinazioneX;
+    private final boolean running;
+    private Pallina p;
 
-    public DatiCondivisi(int numScatole, int inclinazioneXScatola,int inclinazioneYPallina,int inclinazioneXPallina) {
-        this.numScatole = numScatole;
-        this.inclinazioneXScatola = inclinazioneXScatola;
-        this.inclinazioneYPallina= inclinazioneYPallina;
-        this.inclinazioneXPallina=inclinazioneXPallina;
-        
-        running = true;
+    public DatiCondivisi() {
+        this.running = true;
     }
 
-    public synchronized int getNumScatole() {
-
-        return numScatole;
-
+    public synchronized void setValoreInc(int v) {
+        inclinazioneX = v;
     }
 
-    public synchronized int getInclinazioneXScatola() {
-
-        return inclinazioneXScatola;
-
-    }
-    
-    
-    public synchronized int getInclinazioneYPallina() {
-
-        return inclinazioneYPallina;
-
-    }
-    
-    public synchronized int getInclinazioneXPallina() {
-
-        return inclinazioneXPallina;
-
-    }
-    
-     public synchronized void setNumScatole(int numScatole) {
-
-       this.numScatole=numScatole;
-
+    public void resetInc() {
+        inclinazioneX = 0;
     }
 
-    public synchronized void setInclinazioneXScatola(int inclinazioneXScatola) {
-
-        this.inclinazioneXScatola = inclinazioneXScatola;
-
+    public synchronized float getInclinazioneX() {
+        return inclinazioneX;
     }
-    
-     public synchronized void setInclinazioneYPallina(int inclinazioneYPallina) {
-
-        this.inclinazioneYPallina = inclinazioneYPallina;
-
-    }
-    
-      public synchronized void setInclinazioneXPallina(int inclinazioneXPallina) {
-
-        this.inclinazioneXPallina = inclinazioneXPallina;
-
-       
-    }
-    
-    
-    
 
     public boolean isRunning() {
         return running;
+    }
+
+    public synchronized ThScatola getThScatola(int index) {
+        return scatole.get(index);
+    }
+
+    public synchronized void setInclinazione(float inclinazione) {
+        this.inclinazioneX = inclinazione;
+    }
+
+    public synchronized void setScatole(Vector<ThScatola> scatole) {
+        this.scatole = scatole;
+    }
+
+    public Pallina getP() {
+        return p;
+    }
+
+    public synchronized void setP(Pallina p) {
+        this.p = p;
     }
 
 }
