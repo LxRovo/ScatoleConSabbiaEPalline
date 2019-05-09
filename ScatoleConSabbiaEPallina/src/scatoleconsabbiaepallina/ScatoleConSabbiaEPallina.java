a492c0f26cce2eb206504e1f3927cd0b1dde086c
Ziz <Ziz@DESKTOP-FV2DUF0>
2019-05-09 21:14:07 +0200

    Sistemata sabbia

--- ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/ScatoleConSabbiaEPallina.java
+++ ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/ScatoleConSabbiaEPallina.java
@@ -56,6 +56,9 @@ public class ScatoleConSabbiaEPallina extends PApplet {
                 }
                 ThScatola scatola = new ThScatola(altezza, x + (i * larghezza), y + (j * lunghezza), lunghezza, larghezza, sabbia, dati);
                 scatole[i][j] = scatola;
+                
+                scatola.getSabbia().setSize(scatola);
+                
                 if (i == rows / 2 && j == cols / 2) {
                     Pallina p = new Pallina(10, x + scatola.getLarghezza() / 2, scatola.getY() + scatola.getLunghezza() / 2);
                     dati.setP(p);
