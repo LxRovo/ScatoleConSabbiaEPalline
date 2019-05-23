1caab85b5d5db73a6c81622057ff2e2921efa40a
aless <aless@DESKTOP-1MQL8JO>
2019-05-23 19:17:59 +0200

    indendato

--- ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/Sabbia.java
+++ ScatoleConSabbiaEPallina/src/scatoleconsabbiaepallina/Sabbia.java
@@ -1,52 +1,45 @@
-
 package scatoleconsabbiaepallina;
 
-
-
-
 /**
- @author  alessandro_purita
- 
+ * @author alessandro_purita
+ *
  */
 /**
-Classe per gestire la visualizzazione della sabbia ottenendo la sua posizione tramite l'accelerazione, interagisce con la classe ThScatola 
-* * per ottenere le coordinate e con DatiCondivisi per avere l'inclinazione 
+ * Classe per gestire la visualizzazione della sabbia ottenendo la sua posizione
+ * tramite l'accelerazione, interagisce con la classe ThScatola * per ottenere
+ * le coordinate e con DatiCondivisi per avere l'inclinazione
  */
-public class Sabbia   {
-    
+public class Sabbia {
+
     private float volume; //spazio occupato dalla sabbia
     private float accellerazione; //variazione velocità sabbia
     private float posX; //posizione sull'asse x 
     private float posY; //posizione sull'asse y
     DatiCondivisi dati; //puntatore alla classe DatiCondivisi 
-    
 
-    
-
-    
-/**
-     @brief Costruttore con parametri della classe
-
-      costruttore che inizializza la posizione x e y della sabbia passandola come parametro, l'immagine con le dimensioni rispettive
-      * e il volume passate dalla classe ThScatola 
-     * 
-     * 
-     * 
-     * @param volume 
+    /**
+     * @brief Costruttore con parametri della classe
+     *
+     * costruttore che inizializza la posizione x e y della sabbia passandola
+     * come parametro, l'immagine con le dimensioni rispettive e il volume
+     * passate dalla classe ThScatola
+     *
+     *
+     *
+     * @param volume
      * @param posX
      * @param posY
      * @param dati
-     * 
+     *
      */
     public Sabbia(float volume, float posX, float posY, DatiCondivisi dati) {
         this.volume = volume;
         this.accellerazione = 0;
         this.posX = posX;
         this.posY = posY;
-        this.dati = dati;        
+        this.dati = dati;
     }
-    
-    
+
     public float getVolume() {
         return volume;
     }
@@ -62,6 +55,7 @@ public class Sabbia   {
     public float getPosY() {
         return posY;
     }
+
     public void setVolume(float volume) {
         this.volume = volume;
     }
@@ -77,56 +71,42 @@ public class Sabbia   {
     public void setPosY(float posY) {
         this.posY = posY;
     }
-    
-      
-    
-/**
- * @brief metodo per aggiornare la posizione sull'asse x della sabbia
- * 
- * La posizione in x viene calcolata in base all'accelerazione e se la nuova posizione è minore della x minima diviene quella, analogamente con la x massima
-     * 
+
+    /**
+     * @brief metodo per aggiornare la posizione sull'asse x della sabbia
+     *
+     * La posizione in x viene calcolata in base all'accelerazione e se la nuova
+     * posizione è minore della x minima diviene quella, analogamente con la x
+     * massima
+     *
      */
-    public void aggiornaInfo(float inclinazioneX,float larghezza)
-    {
-    double x = Math.toRadians(dati.getSens().getInclinazioneX());   
-    accellerazione = (float) (9.8 * Math.sin(x));
-    posX= posX+accellerazione;
-    if (inclinazioneX <= 15) 
-    {
-     posX= posX+accellerazione;
-    if(posX<dati.getMinX())
-    {
-    posX=dati.getMinX()+10;
-    
-    }
-    if(posX>dati.getMaxX())
-    {
-    posX=dati.getMaxX()-10;
-    
-    }
-    }
-    if (inclinazioneX >= 15) 
-    {
-        posX= posX+accellerazione;
-    if(posX<dati.getMinX())
-    {
-    posX=dati.getMinX();
-    
-    }
-    if(posX>dati.getMaxX())
-    {
-    posX=dati.getMaxX();
-    
-    }
-    }
-    
-    
-        
-    
-    
-    }
+    public void aggiornaInfo(float inclinazioneX, float larghezza) {
+        double x = Math.toRadians(dati.getSens().getInclinazioneX());
+        accellerazione = (float) (9.8 * Math.sin(x));
+        posX = posX + accellerazione;
+        if (inclinazioneX <= 15) {
+            posX = posX + accellerazione;
+            if (posX < dati.getMinX()) {
+                posX = dati.getMinX() + 10;
 
-   
+            }
+            if (posX > dati.getMaxX()) {
+                posX = dati.getMaxX() - 10;
 
+            }
+        }
+        if (inclinazioneX >= 15) {
+            posX = posX + accellerazione;
+            if (posX < dati.getMinX()) {
+                posX = dati.getMinX();
+
+            }
+            if (posX > dati.getMaxX()) {
+                posX = dati.getMaxX();
+
+            }
+        }
+
+    }
 
 }
